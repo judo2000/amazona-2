2cbdefa12e53b209eb1c1bf907ae05f28cdc2239
Stephen Moore <judo2000@comcast.net>
2022-12-24 22:33:47 -0700

    Import font-awsome and create product and rating components

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Rating from './Rating';
+
+const Product = (props) => {
+  const { product } = props;
+  return (
+    <Card>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+          <Rating rating={product.rating} numReviews={product.numReviews} />
+        </Link>
+        <Card.Text>${product.price}</Card.Text>
+        <Button>Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
