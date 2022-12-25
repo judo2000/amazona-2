12a6bc1facaf434727a34943ccff1b74d6958eb7
Stephen Moore <judo2000@comcast.net>
2022-12-24 22:01:55 -0700

    Install react-bootstrap, bootstrap, and react-router-bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,6 @@
 import { useEffect, useReducer } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import logger from 'use-reducer-logger';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -18,7 +17,7 @@ const reducer = (state, action) => {
 
 const HomeScreen = () => {
   //const [products, setProducts] = useState([]);
-  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
     error: '',
