1a453ccdb9d5d378839d65d513a8c5c9d4a88305
Stephen Moore <judo2000@comcast.net>
2022-12-26 15:17:11 -0700

    Seed database with sample users

--- backend/controllers/seedController.js
+++ backend/controllers/seedController.js
@@ -1,12 +1,13 @@
 import Product from '../models/ProductModel.js';
+import User from '../models/UserModel.js';
 import data from '../data.js';
 
 const seedProducts = async (req, res) => {
   await Product.deleteMany({});
   const createdProducts = await Product.insertMany(data.products);
-  //await User.remove({});
-  //const createdUsers = await User.insertMany(data.users);
-  res.send({ createdProducts });
+  await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 };
 
 export { seedProducts };
--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Stephen',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: 'true',
+    },
+    {
+      name: 'Scott',
+      email: 'scott@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: 'false',
+    },
+  ],
   products: [
     {
       //_id: '1',
