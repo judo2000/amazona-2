0327e91e031f281f59b35e77e23948497a0651f7
Stephen Moore <judo2000@comcast.net>
2023-01-02 18:13:30 -0700

    Prepare for deployment to heroku and render

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import connectDB from './config/connection.js';
 
@@ -6,7 +7,7 @@ import seedRoutes from './routes/seedRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-//import data from './data.js';
+
 dotenv.config();
 
 connectDB();
@@ -19,14 +20,16 @@ app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 
-// app.get('/', (req, res) => {
-//   res.send('API is running...');
-// });
-
 app.use('/api/seed', seedRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(Path.join(__dirname, '/frontend/build/index.html'));
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
