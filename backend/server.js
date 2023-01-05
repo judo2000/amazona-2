ab157d9497c473f302bd3591e902be62be830aad
Stephen Moore <judo2000@comcast.net>
2023-01-05 15:40:02 -0700

    Update server.js to try to get this working on heroku

--- backend/server.js
+++ backend/server.js
@@ -25,18 +25,12 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('(', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('*', (req, res) => res.send('Please set to production'));
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
@@ -44,5 +38,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
