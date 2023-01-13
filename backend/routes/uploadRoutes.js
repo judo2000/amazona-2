cd8e6b94060bdb99cd9b1cc9040c768d9e14878f
Stephen Moore <judo2000@comcast.net>
2023-01-13 05:21:51 -0700

    Updated updateRouter.js to see if I can get this working on heroku

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,6 +1,26 @@
+// import express from 'express';
+// import multer from 'multer';
+// import { uploadProductImg } from '../controllers/uploadController.js';
+// import { isAdmin, isAuth } from '../utils.js';
+
+// const upload = multer();
+
+// const uploadRouter = express.Router();
+
+// uploadRouter.post(
+//   '/',
+//   isAuth,
+//   isAdmin,
+//   upload.single('file'),
+//   uploadProductImg
+// );
+
+// export default uploadRouter;
+
 import express from 'express';
 import multer from 'multer';
-import { uploadProductImg } from '../controllers/uploadController.js';
+import { v2 as cloudinary } from 'cloudinary';
+import streamifier from 'streamifier';
 import { isAdmin, isAuth } from '../utils.js';
 
 const upload = multer();
@@ -12,7 +32,26 @@ uploadRouter.post(
   isAuth,
   isAdmin,
   upload.single('file'),
-  uploadProductImg
+  async (req, res) => {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    const streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+    const result = await streamUpload(req);
+    res.send(result);
+  }
 );
-
 export default uploadRouter;
