c143d07b92720238605061042da695cd8d364801
Stephen Moore <judo2000@comcast.net>
2023-01-15 21:25:10 -0700

    Show the address selected in shipping on map in order page

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -210,6 +210,16 @@ const OrderScreen = () => {
                 <strong>Address: </strong> {order.shippingAddress.address},
                 {order.shippingAddress.city}, {order.shippingAddress.postalCode}
                 , {order.shippingAddress.country}
+                &nbsp;
+                {order.shippingAddress.location.lat &&
+                  order.shippingAddress.location.lng && (
+                    <a
+                      target="_new"
+                      href={`https://maps.google.com?q=${order.shippingAddress.location.lat},${order.shippingAddress.location.lng}`}
+                    >
+                      Show on Map
+                    </a>
+                  )}
               </Card.Text>
               {order.isDelivered ? (
                 <MessageBox variant="success">
